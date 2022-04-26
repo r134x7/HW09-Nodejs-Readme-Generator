0fdf57b260ef52128293f07a5e8b793409f05f89
r134x7 <r134x7@gmx.com>
2022-04-26 18:18:34 +1000

    adjustments to generating license section.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -33,14 +33,10 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license.includes("MIT")) {
-    return "Licensed under the MIT License."
-  } else if (license.includes("Apache")) {
-    return "Licensed under the Apache 2.0 License."
-  } else if (license.includes("Mozilla")) {
-    return "Licensed under the Mozilla Public License 2.0."
-  } else if (license.includes("GNU")) {
-    return "Licensed under the GNU General Public License v3.0"
+  if (!license.includes("No License used.")) {
+    return `# License
+    
+    Licensed under the `
   } else {
     return "";
   }
@@ -78,12 +74,8 @@ function generateMarkdown(data) {
   # Usage
   
   ${data.usage}
-
-  # License
   
-  ${section}
-  
-  ${link}
+  ${section}${link}
 
   # Contributing
   
